8be10261f39aa37005648a48a26a3881d7f2425f
bitchrussiankid <andriuchajetga56@gmail.com>
2025-12-19 17:48:24 +0300

    (Lecture 1) Task 8

---
+++ ZabelkinPyProject01/lecture1Task8/lecture1Task8.py
@@ -0,0 +1,8 @@
+x = 15
+y = 4
+
+print(f"Sum of numbers = {x + y}")
+print(f"Difference of numbers = {x - y}")
+print(f"Multiplication of numbers = {x * y}")
+print(f"Integer division of numbers = {x // y}")
+print(f"Remainder of division of numbers = {x % y}")
